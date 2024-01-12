c7088781884164abe7d78449188a9b1f6c6b2528
Kashif <ahmed.kashif1001@gmail.com>
2024-01-12 22:12:54 +0500

    function returing a string

--- DataTypes/functions.ts
+++ DataTypes/functions.ts
@@ -1,4 +1,13 @@
-let sayHi () => {
+let sayHi = () => {
     console.log("Hello, this is a function");
+}
+
+sayHi();
+
+//the function above returns a type void, we can return numbers or strings by the following example
+
+let funcReturnstring = () :string=>{
+    console.log("Hi");
+    return "lama dev"
     
 }
