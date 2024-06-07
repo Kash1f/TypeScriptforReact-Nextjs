6101296bc12aa11a5816806c7e111e383de83040
Kashif <ahmed.kashif1001@gmail.com>
2024-06-07 19:15:02 +0500

    typescript basics

---
+++ TypeScript Basics/index.js
@@ -0,0 +1,5 @@
+var fullName = "jack";
+console.log(fullName);
+var names = []; //this is an array for a string so a number cannot be assigned to it
+names.push("hi");
+console.log(names);
---
+++ TypeScript Basics/index.ts
@@ -0,0 +1,8 @@
+let fullName : string = "jack"
+console.log(fullName);
+
+
+const names: string[] = []; //this is an array for a string so a number cannot be assigned to it
+names.push("hi")
+
+console.log(names);
