0d8d457bf9218e64611d879376fc4c265d992709
Kashif <ahmed.kashif1001@gmail.com>
2024-06-08 00:13:01 +0500

    unions and enums

--- TypeScript for React/dist/index.js
+++ TypeScript for React/dist/index.js
@@ -5,4 +5,16 @@ let company = "Ky Labs";
 let isPublishes = true;
 let abc = "Hello";
 let ids = [1, 2, 3];
-let id;
+//Union : can hold multiple types
+let uid;
+uid = '22';
+// uid = "Hello"
+//Enums: Enums allow us to define a set of named constants either numeric or string
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Right"] = 2] = "Right";
+    Direction1[Direction1["Left"] = 3] = "Left";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up);
--- TypeScript for React/src/index.ts
+++ TypeScript for React/src/index.ts
@@ -7,4 +7,20 @@ let abc: any = "Hello";
 
 let ids: number[] = [1,2,3]
 
-let id: string|number
+
+//Union : can hold multiple types
+let uid: string|number
+uid = '22'
+// uid = "Hello"
+
+//Enums: Enums allow us to define a set of named constants either numeric or string
+
+enum Direction1 {
+    Up,
+    Down,
+    Right,
+    Left
+}
+
+console.log(Direction1.Up);
+
